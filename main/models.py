32f40b15bd1ddaec523f431bacff28ff04fcceb3
Gionado <akusaya0201@gmail.com>
2025-09-15 20:19:56 +0700

    penyelesaian tugas 3, penambahan fungsi di views.py dalam format XML, JSON, XML by ID, dan JSON by ID

--- main/models.py
+++ main/models.py
@@ -10,20 +10,20 @@ class Product(models.Model):
         ('lain lain', 'Lain Lain'),
     ]
 
-    name = models.CharField
+    name = models.CharField(max_length=255)
     price = models.PositiveIntegerField(default=0)
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
     category = models.CharField(choices=pilihan_category, default='update')
-    is_featured = models.BooleanField
+    is_featured = models.BooleanField(default = False)
     
     def __str__(self):
         return self.name
     
-    @property
-    def is_news_hot(self):
-        return self.news_views > 20
+    # @property
+    # def is_product_hot(self):
+    #     return self.product_views > 20
         
-    def increment_views(self):
-        self.news_views += 1
-        self.save()
+    # def increment_views(self):
+    #     self.product_views += 1
+    #     self.save()
--- tokoku/settings.py
+++ tokoku/settings.py
@@ -31,6 +31,9 @@ DEBUG = True
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","gionado-gunawan-tokoku.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://gionado-gunawan-tokoku.pbp.cs.ui.ac.id"
+]
 
 # Application definition
 
@@ -59,7 +62,7 @@ ROOT_URLCONF = 'tokoku.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
